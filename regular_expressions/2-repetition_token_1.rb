bdd2370b73813c7dd1127498cbefba0253834efb
AR-JUNA <a.gyan@alustudent.com>
2025-10-26 10:02:05 +0400

    Update 2-repetition_token_1.rb

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,16 +1,2 @@
 #!/usr/bin/env ruby
-# 2-repetition_token_1.rb
-# This script accepts one argument and prints all occurrences of a specific pattern:
-# 'h' followed by 0, 1, 2, or more 'b's (specifically matching htn and hbtn).
-
-input_string = ARGV[0] || ""
-
-# Correct regex to match:
-# h followed by 0 or 1 'b', then 'tn'
-regex = /hb?tn/
-
-# Find all matches
-matches = input_string.scan(regex)
-
-# Print concatenated result
-puts matches.join
+puts ARGV[0].scan(/hb?tn/).join
