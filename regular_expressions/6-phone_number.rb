780750d3ad47302bcd11d7a634dea4bc05e60b68
AR-JUNA <a.gyan@alustudent.com>
2025-10-26 10:57:00 +0400

    Update 6-phone_number.rb

--- regular_expressions/6-phone_number.rb
+++ regular_expressions/6-phone_number.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-# File: 6-phone_number.rb
-
-# Requirement: Match a 10 digit phone number, optionally separated by space or hyphen.
-# Pattern: ^(3 digits)(optional separator)(3 digits)(optional separator)(4 digits)$
-regex = /^\d{3}[- ]?\d{3}[- ]?\d{4}$/
-
-input_string = ARGV[0]
-
-if input_string
-  # Use scan to return the full matched string
-  matches = input_string.scan(regex)
-  puts matches.join
-end
+puts ARGV[0].scan(/^\d{10}$/).join
