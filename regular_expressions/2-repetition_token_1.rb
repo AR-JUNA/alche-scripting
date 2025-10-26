002643db37302758e625fdf8958483dec1731489
AR-JUNA <a.gyan@alustudent.com>
2025-10-26 09:54:53 +0400

    Update 2-repetition_token_1.rb

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,2 +1,22 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb?tn/).join
+# 2-repetition_token_1.rb
+# This script accepts one argument and prints all occurrences of a specific pattern:
+# 'h' followed by 0, 1, 3, or 4 'b's, followed by 'tn'.
+
+input_string = ARGV[0] || ""
+
+# The regular expression to match:
+# 'h'
+# (?:...) is a non-capturing group for alternatives:
+#   '' (empty string) for 0 'b's
+#   'b' for 1 'b'
+#   'b{3}' for 3 'b's
+#   'b{4}' for 4 'b's
+# 'tn'
+regex = /h(?:|b|b{3}|b{4})tn/
+
+# Find all non-overlapping matches in the input string
+matches = input_string.scan(regex)
+
+# Concatenate all found matches and print to standard output
+puts matches.join
