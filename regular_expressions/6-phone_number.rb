060c70fcadcaa393297c4843e81bb02980de32d0
AR-JUNA <a.gyan@alustudent.com>
2025-10-26 10:54:07 +0400

    Update 6-phone_number.rb

--- regular_expressions/6-phone_number.rb
+++ regular_expressions/6-phone_number.rb
@@ -1,2 +1,11 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/^\d{10}$/).join
+#!/usr/bin/ruby
+
+input = ARGV[0]
+regex = /\A\s*(\d{3}[-.\s]?\d{3}[-.\s]?\d{4}|\d{10})\s*\z/
+match = regex.match(input)
+
+if match
+  puts match[1]  # Print the captured group (the 10-digit phone number)
+else
+  puts "No match"
+end
