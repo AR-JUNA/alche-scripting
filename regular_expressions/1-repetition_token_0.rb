b0e94b683d6fad689b5b56dbd394cf52b614366b
Arjuna Gyana <a.gyan@alustudent.com>
2025-10-25 22:30:56 -0700

    1-repetition_token_0.rb: Implement regex for zero or more 't's; update README

---
+++ regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# 1-repetition_token_0.rb
+# This script accepts one argument and prints all occurrences of a specific pattern:
+# 'hb' followed by zero or more 't's, followed by 'n'.
+
+input_string = ARGV[0] || ""
+
+# The regular expression to match: h, b, zero or more t's, n
+# 't*' means zero or more 't' characters.
+regex = /hbt*n/
+
+# Find all non-overlapping matches in the input string
+matches = input_string.scan(regex)
+
+# Concatenate all found matches and print to standard output
+puts matches.join
