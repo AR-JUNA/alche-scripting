55d3d23bfbfe8c5a1ecad25c0a971fdcdf62e5e6
AR-JUNA <a.gyan@alustudent.com>
2025-10-26 09:57:11 +0400

    Update 2-repetition_token_1.rb

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -8,12 +8,12 @@ input_string = ARGV[0] || ""
 # The regular expression to match:
 # 'h'
 # (?:...) is a non-capturing group for alternatives:
-#   '' (empty string) for 0 'b's
+#   'b{0}' for 0 'b's (explicitly zero 'b's)
 #   'b' for 1 'b'
 #   'b{3}' for 3 'b's
 #   'b{4}' for 4 'b's
 # 'tn'
-regex = /h(?:|b|b{3}|b{4})tn/
+regex = /h(?:b{0}|b|b{3}|b{4})tn/ # <-- THIS LINE IS CHANGED
 
 # Find all non-overlapping matches in the input string
 matches = input_string.scan(regex)
