cf3b6bd42c90cbef1c38a8d4d1df35df370a7815
Arjuna Gyana <a.gyan@alustudent.com>
2025-10-25 22:35:33 -0700

    Fix: 1-repetition_token_0.rb regex to match 0 to 7 't's as per test cases

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 # 1-repetition_token_0.rb
 # This script accepts one argument and prints all occurrences of a specific pattern:
-# 'hb' followed by zero or more 't's, followed by 'n'.
+# 'hb' followed by zero to seven 't's, followed by 'n'.
 
 input_string = ARGV[0] || ""
 
-# The regular expression to match: h, b, zero or more t's, n
-# 't*' means zero or more 't' characters.
-regex = /hbt*n/
+# The regular expression to match: h, b, zero to seven t's, n
+# 't{0,7}' means zero to seven 't' characters.
+regex = /hbt{0,7}n/
 
 # Find all non-overlapping matches in the input string
 matches = input_string.scan(regex)
